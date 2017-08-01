ebb4d6deb22de7e4edc532d4c8f74a0e552e8c97
homovegetus <gabrielecalissi@live.it>
2017-08-01 19:00:13 +0200

    Update FiglioDiUnPanda.py

--- FiglioDiUnPanda.py
+++ FiglioDiUnPanda.py
@@ -1,11 +1,8 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import random
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-var port = process.env.PORT || 443;
-var host = process.env.HOST;
-var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(bot, update):
     update.message.reply_text('Incredibile, anche un pirla come te sa parlare!')
