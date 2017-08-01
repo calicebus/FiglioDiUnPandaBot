c24a43429b571cf3e34c0024b74b00f9d24322ef
homovegetus <gabrielecalissi@live.it>
2017-08-01 17:09:23 +0200

    Update FiglioDiUnPanda.py

--- FiglioDiUnPanda.py
+++ FiglioDiUnPanda.py
@@ -3,7 +3,7 @@ import random
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-var port = process.env.PORT || 8443;
+var port = process.env.PORT or 8443;
 var host = process.env.HOST;
 var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
 
